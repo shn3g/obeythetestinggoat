d810d31cba79e43410e1081475a29dcdec09ce9d
Piotr Szczesniak <piotr.szczesniak@mainstreamrp.com>
2022-08-23 09:28:46 +0100

    1st proper test case

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
-# from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.common.by import By
+import unittest
 
-# options = Options()
-# options.binary_location = r'C:\Users\Piotr.Szczesniak\AppData\Local\Mozilla\firefox.exe'
-browser = webdriver.Edge()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):  
+        self.browser = webdriver.Edge()
 
-assert 'Django' in browser.title
+    def tearDown(self):  
+        self.browser.quit()
 
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)  
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':  
+    unittest.main()
